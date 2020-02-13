4d05deb85dc4fe27298115a1d250303b6964d874
Calvin Nguyen <calvinnguyen50@gmail.com>
2020-02-13 23:33:45 +0000

    removed agrs and exported

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,24 +1,19 @@
 const request = require('request');
 const data = 'https://api.thecatapi.com/v1/breeds/search?q=';
-let args = process.argv;
-let result = args.slice(2);
-let breed = result[0];
+// let args = process.argv;
+// let result = args.slice(2);
+// let breed = result[0];
 
-
-request(`${data}${breed}` , (error, response, body) => {
-  if (error) {
-    throw error;
-  }
-
-  if (!body) {
-    console.log('BREED IS NOT FOUND!!!');
-  }
-
-  const data = JSON.parse(body);
-  console.log(data[0].description);
-  // console.log(typeof data);
-
+const fetchBreedDescription = (breedName, callback) => {
+  request(`${data}${breedName}` , (error, response, body) => {
+    const data = JSON.parse(body);
     
+    if (data.length === 0) {
+        callback(error, null);
+    } else {
+        callback(null, data[0].description.trim());
+    }
+  });
+};
 
-});
-
+module.exports = { fetchBreedDescription };
